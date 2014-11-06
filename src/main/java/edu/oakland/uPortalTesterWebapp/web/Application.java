90e4c8ebde510768feef6da0a73bc6be2c75b395
dcfreema <dcfreema@oakland.edu>
2014-11-06 08:10:05 -0500

    Begin to add proper logging

--- src/main/java/edu/oakland/uPortalTesterWebapp/web/Application.java
+++ src/main/java/edu/oakland/uPortalTesterWebapp/web/Application.java
@@ -2,6 +2,7 @@ package edu.oakland.uPortalTesterWebapp.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,11 +31,17 @@ public class Application {
     @RequestMapping(value="/", method=RequestMethod.GET)
     public ModelAndView home(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView home = new ModelAndView("home");
-        home.addObject("running", getRunning());
+        home.addObject("running", getRunning(request));
         return home;
     }
 
-    private boolean getRunning() {
+    private boolean getRunning(HttpServletRequest request) {
+
+		String ipAddress = request.getRemoteAddr();
+		if(log.isDebugEnabled()) {
+			log.debug("TEST");
+		}
+
 	    Document document = getDocument("http://localhost:8080/uPortal");
 	    if ( ( document.getElementById("portal").html() ).length() > 0 ) {
 		    return true;
